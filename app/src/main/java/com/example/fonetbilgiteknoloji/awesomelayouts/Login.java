942372cd06ea5c8292ede7eec70a9acefcbbde63
ytam <tamyldrm@gmail.com>
2018-10-30 21:49:39 +0300

    Added login screen

---
+++ app/src/main/java/com/example/fonetbilgiteknoloji/awesomelayouts/Login.java
@@ -0,0 +1,12 @@
+package com.example.fonetbilgiteknoloji.awesomelayouts;
+
+import android.os.Bundle;
+
+public class Login extends RootActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+    }
+}
